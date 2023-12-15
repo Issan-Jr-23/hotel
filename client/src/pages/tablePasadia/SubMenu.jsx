e01b3ee46bdc3e60f5788f97e9292343fe97fc8e
Isan Martinez <101842167+Issan-Jr-23@users.noreply.github.com>
2023-12-15 13:47:12 -0500

    ajustes

--- client/src/pages/tablePasadia/SubMenu.jsx
+++ client/src/pages/tablePasadia/SubMenu.jsx
@@ -1,87 +1,73 @@
-import React, {useState, useEffect} from "react";
-import { Modal, ModalContent, ModalHeader, ModalBody, Select, SelectItem, ModalFooter, Button, useDisclosure, Checkbox, Input, Link } from "@nextui-org/react";
-import { MailIcon } from "../iconos/MailIcon.jsx";
-import { LockIcon } from "../iconos/LockIcon.jsx";
+import React, { useState, useEffect } from "react";
+import { Input, Select, SelectItem } from "@nextui-org/react";
 import AxiosInstance from "../../api/axios.js";
 
 export default function App() {
-    const { isOpen, onOpen, onOpenChange } = useDisclosure();
-    const [value, setValue] = React.useState(new Set([]));
-    const [touched, setTouched] = React.useState(false);
-    const [snacks , setSnacks] = useState([])
+    const [snacks, setSnacks] = useState([]);
+    const [selectedSnack, setSelectedSnack] = useState(null);
 
-    const isValid = value.has("cat");
-    
     useEffect(() => {
         const fetchData = async () => {
-          try {
-            const response = await AxiosInstance.get("/food");
-            const allProducts = response.data;
-      
-            const subProducts = allProducts.reduce((acc, product) => {
-              if (product.subproductos && product.subproductos.length > 0) {
-                acc.push(...product.subproductos);
-              }
-              return acc;
-            }, []);
-      
-            setSnacks(subProducts); 
-          } catch (error) {
-            console.error("Error al obtener datos del servidor:", error);
-          }
+            try {
+                const response = await AxiosInstance.get("/food");
+                const allProducts = response.data;
+
+                let subProducts = [];
+                allProducts.forEach(product => {
+                    if (product.subproductos) {
+                        const subProductosConCantidadPadre = product.subproductos.map(sub => {
+                            return { ...sub, cantidadPadre: product.CantidadInicial };
+                        });
+                        subProducts = subProducts.concat(subProductosConCantidadPadre);
+                    }
+                });
+
+                setSnacks(subProducts);
+            } catch (error) {
+                console.error("Error al obtener datos del servidor:", error);
+            }
         };
         fetchData();
-      }, []);
+    }, []);
+
+    const handleSelectChange = (e) => {
+        const selectedId = e; // Aquí puede necesitar ajustar según cómo el Select maneja el valor
+        const selected = snacks.find(snack => snack._id === selectedId);
+        setSelectedSnack(selected);
+    };
 
     return (
-        <>
-            <div className="pl-5 pr-5">
-                <div className="flex">
-                    <Input
-                        autoFocus
-                        endContent={
-                            <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                        }
-                        label="Email"
-                        placeholder="Enter your email"
-                        variant="bordered"
-                        className="mr-2 w-96"
-                    />
-                    <Input
-                        label="Stock"
-                        placeholder=""
-                        type="password"
-                        variant="bordered"
-                        className="w-32 mr-2"
-                    />
-                    <Select
-                        label="Favorite Animal"
-                        variant="bordered"
-                        placeholder="Select an animal"
-                        description="The second most popular pet in the world"
-                        errorMessage={isValid || !touched ? "" : "You must select a cat"}
-                        isInvalid={isValid || !touched ? false : true}
-                        selectedKeys={value}
-                        className="max-w-xs"
-                        onSelectionChange={setValue}
-                        onClose={() => setTouched(true)}
-                    >
-                        {snacks.map((food) => (
-                            <SelectItem key={food._id}>
-                                {food.Descripcion}
-                            </SelectItem>
-                        ))}
-                    </Select>
-                    <Input
-                        label="Stock"
-                        placeholder=""
-                        type="password"
-                        variant="bordered"
-                        className="w-32 ml-2"
-                    />
-                </div>
-
+        <div className="pl-5 pr-5">
+            <div className="flex">
+                <Input
+                    label="Ingrese una cantidad"
+                    placeholder="Enter your email"
+                    variant="bordered"
+                    className="mr-2 w-96"
+                    type="Number"
+                />
+                <Input
+                    disabled
+                    label="Stock"
+                    value={selectedSnack ? selectedSnack.cantidadPadre : ''}
+                    readOnly
+                    variant="bordered"
+                    className="w-32 mr-2"
+                />
+                <Select
+                    label="Subproducto"
+                    variant="bordered"
+                    placeholder="Select a subproduct"
+                    className="max-w-xs"
+                    onChange={(e) => handleSelectChange(e.target.value)}
+                >
+                    {snacks.map((snack) => (
+                        <SelectItem key={snack._id} value={snack._id}>
+                            {snack.Descripcion}
+                        </SelectItem>
+                    ))}
+                </Select>
             </div>
-        </>
+        </div>
     );
 }
