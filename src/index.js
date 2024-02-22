6c6ff8e854e00437f457fca68d6346adcbb18def
Isan Martinez <101842167+Issan-Jr-23@users.noreply.github.com>
2024-02-22 08:18:03 -0500

    cambios para produccion

--- src/index.js
+++ src/index.js
@@ -1,32 +1,12 @@
-import app from "./app.js";
-import { PORT } from "./config.js";
-import { connectDB } from "./db.js";
-
-async function main() {
-  try {
-    await connectDB();
-    app.listen(PORT);
-    console.log(`Listening on port http://localhost:${PORT}`);
-    console.log(`Environment: ${process.env.NODE_ENV}`)
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-main();
-
-
-
 // import app from "./app.js";
 // import { PORT } from "./config.js";
 // import { connectDB } from "./db.js";
-// import https from "https"
 
 // async function main() {
 //   try {
 //     await connectDB();
-//     app.listen(8080, console.log("Server running"));
-//     console.log(`Listening on port http://localhost:8080`);
+//     app.listen(PORT);
+//     console.log(`Listening on port http://localhost:${PORT}`);
 //     console.log(`Environment: ${process.env.NODE_ENV}`)
 //   } catch (error) {
 //     console.error(error);
@@ -34,3 +14,23 @@ main();
 // }
 
 // main();
+
+
+
+import app from "./app.js";
+import { PORT } from "./config.js";
+import { connectDB } from "./db.js";
+import https from "https"
+
+async function main() {
+  try {
+    await connectDB();
+    app.listen(8080, console.log("Server running"));
+    console.log(`Listening on port http://localhost:8080`);
+    console.log(`Environment: ${process.env.NODE_ENV}`)
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
